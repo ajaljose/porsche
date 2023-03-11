b08ee42c5500b17eafc4c8fed1f7b08d4c4987d1
Ajal Jose <ajaljosem@gmail.com>
2023-03-11 23:40:54 +0530

    navbar started

---
+++ src/home/navbar.js
@@ -0,0 +1,10 @@
+import './navbar.css';
+const Navbar=()=>{
+return <div className="nav">
+<h2 className="mainHeader">Porsche</h2>
+<img src="../images/porsche.png" alt="porsche"  width="500" height="600"/>
+<h4 className='navMenuIcon'>III</h4>
+<h4 className="navMenu">Menu</h4>
+</div>
+}
+export default Navbar;
