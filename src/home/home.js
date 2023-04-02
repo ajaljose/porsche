e73b786b7118bd57a26892167c8bd7f84681741d
Ajal Jose <ajaljosem@gmail.com>
2023-04-02 18:18:16 +0530

    minor style change in prev components

--- src/home/home.js
+++ src/home/home.js
@@ -9,12 +9,7 @@ const Home = () => {
       
       
       <img  src={logo} alt="porsche" width="46%"  title="PORSCHE"/>
-      <button className="cta">
-  <span>Read More</span>
-  <svg viewBox="0 0 13 10" height="10px" width="15px">
-    <path d="M1,5 L11,5"></path>
-    <polyline points="8 1 12 5 8 9"></polyline>
-  </svg></button>
+      
   <svg width="770" id="bgStrip"className="background"height="800" viewBox="0 0 691 800" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fillRule="evenodd" clipRule="evenodd" d="M0.424896 800L465.41 0H690.322L225.336 800H0.424896Z" fill="black"/>
 </svg>
