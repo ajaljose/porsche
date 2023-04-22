73baa1d422f1b3ca2ebcf5b0e54d78015bee08dc
Ajal Jose <ajaljosem@gmail.com>
2023-04-22 21:10:10 +0530

    minor

--- src/home/scripts.js
+++ src/home/scripts.js
@@ -4,9 +4,7 @@ function App() {
         <Header/>
         <Banner/>
         <RowPost url={originals} title='Originals' />
-        <RowPost url={action} title='Action' isSmall/>
-        <RowPost url={romance} title='Romance' />
-        <RowPost url={animation} title='Animation' isSmall/>
+       
         <Footer/>
   
       </div>
