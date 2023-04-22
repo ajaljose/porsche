3cd1545812438378d8281ca365f3c0fab3b986ac
Ajal Jose <ajaljosem@gmail.com>
2023-04-22 21:11:29 +0530

    formatted document

--- src/home/navbar.js
+++ src/home/navbar.js
@@ -1,12 +1,12 @@
-import './navbar.css';
-import logo from '../images/porschelogo.png';
+import "./navbar.css";
+import logo from "../images/porschelogo.png";
 // import font from url('https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Open+Sans&family=Raleway:wght@100&display=swap');
 
 const Navbar = () => {
   return (
     <div className="nav foreground">
-      <img src={logo} alt="porsche" width="50" height="50" title="PORSCHE"/>
-      <h3 className='prcFont'>TAYCAN TURBO S</h3>
+      <img src={logo} alt="porsche" width="50" height="50" title="PORSCHE" />
+      <h3 className="prcFont">TAYCAN TURBO S</h3>
       <h4 className="navMenu prcFont">Menu</h4>
     </div>
   );
