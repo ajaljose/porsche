7986d9a7ec59ea8e507320b26e872fb8ac5d4a1b
Ajal Jose <ajaljosem@gmail.com>
2023-04-29 21:12:43 +0530

    right navbar code not aligned css pending

---
+++ src/home/rightNavbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './rightNavbar.css'
+function rightNavbar() {
+  return (
+    <div>
+        <table>
+            <tr><td>Settings 1</td></tr>
+            <tr><td>Settings 2</td></tr>
+            <tr><td>Settings 3</td></tr>
+            <tr><td>Settings 4</td></tr>
+            <tr><td>Settings 5</td></tr>            
+        </table>
+    </div>
+  )
+}
+
+export default rightNavbar
