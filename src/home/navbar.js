e448b7eecead434acb4bf14e23c5b0d309c00cf0
Ajal Jose <ajaljosem@gmail.com>
2023-04-29 21:12:24 +0530

    added hamburger icon to navbar

--- src/home/navbar.js
+++ src/home/navbar.js
@@ -1,5 +1,6 @@
 import "./navbar.css";
 import logo from "../images/porschelogo.png";
+import hamlogo from "../images/hamburger.png";
 // import font from url('https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Open+Sans&family=Raleway:wght@100&display=swap');
 
 const Navbar = () => {
@@ -7,7 +8,7 @@ const Navbar = () => {
     <div className="nav foreground">
       <img src={logo} alt="porsche" width="50" height="50" title="PORSCHE" />
       <h3 className="prcFont">TAYCAN TURBO S</h3>
-      <h4 className="navMenu prcFont">Menu</h4>
+      <h4 className="navMenu prcFont">Menu <img className="settings" src={hamlogo} /></h4>
     </div>
   );
 };
