98f035833765b36c1272a2e16255e53a81f1d170
Ajal Jose <ajaljosem@gmail.com>
2023-05-03 20:50:21 +0530

    close RHnavbar on close btn

--- src/home/rightNavbar.jsx
+++ src/home/rightNavbar.jsx
@@ -8,7 +8,12 @@ function rightNavbar() {
             <tr><td>Settings 2</td></tr>
             <tr><td>Settings 3</td></tr>
             <tr><td>Settings 4</td></tr>
-            <tr><td>Settings 5</td></tr>            
+            <tr><td>Settings 5</td></tr> 
+            <tr><td onClick={() => {
+  const rightNavbar = document.querySelector('.right-navbar');
+  rightNavbar.style.right = '-200px';
+  rightNavbar.style.transition = 'right 0.6s ease-in-out'; // adjust the transition duration and timing function to your preference
+}}>Close</td></tr>            
         </table>
     </div>
   )
