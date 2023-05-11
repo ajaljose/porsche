3b583ade75119729491635f9b3f81a7bdb314196
Ajal Jose <ajaljosem@gmail.com>
2023-05-11 21:28:38 +0530

    footer

--- src/home/footer.jsx
+++ src/home/footer.jsx
@@ -1,54 +1,61 @@
-import React from 'react'
-// import './footer.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap';
-function footer() {
+import React from 'react';
+import './footer.css';
+const Footer = () => {
   return (
-    <div>
-      {/* <Button variant="primary">Click me</Button> */}
-      <footer className="footer">
-  	 <div className="container">
-  	 	<div className="row">
-  	 		<div className="footer-col">
-  	 			<h4>company</h4>
-  	 			<ul>
-  	 				<li><p >our services</p></li>
-  	 				<li>< p>privacy policy</p></li>
-  	 				<li><p>affiliate program</p></li>
-  	 				<li><p>about us</p></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div className="footer-col">
-  	 			<h4>get help</h4>
-  	 			<ul>
-  	 				<li><p>FAQ</p></li>
-  	 				<li><p>shipping</p></li>
-  	 				<li><p>returns</p></li>
-  	 				<li><p>order status</p></li>
-  	 			</ul>
-  	 		</div>
-  	 		
-  	 		<div className="footer-col">
-  	 			<h4>follow us</h4>
-  	 			<ul>
-					
-  	 				<li><p>Facebook</p></li>
-  	 				<li><p>Twitter</p></li>
-  	 				<li><p>Instagram</p></li>
-  	 				<li><p>LinkedIn</p></li>
-  	 			</ul>
-  	 		</div>
-         <div className="footer-col">
-  	 			<h4>Disclaimer</h4>
-  	 			<p id='Disclaimer'>This site is for educational purposes only!!
-Copyright Disclaimer under section 107 of the Copyright Act 1976, allowance is made for “fair use” for purposes such as  teaching, education and research etc...</p>
-  	 		</div>
-  	 	</div>
-       <p id='Disclaimer'>Created By Ajal Jose</p>
-  	 </div>
-  </footer>
-    </div>
-  )
-}
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p className="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL, and Algorithm.</p>
+          </div>
 
-export default footer
+          <div className="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul className="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+            </ul>
+          </div>
+
+          <div className="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul className="footer-links">
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-6 col-xs-12">
+            <p className="copyright-text">
+              Copyright &copy; 2017 All Rights Reserved by
+              <a href="#">Scanfcode</a>.
+            </p>
+          </div>
+
+          <div className="col-md-4 col-sm-6 col-xs-12">
+            <ul className="social-icons">
+              <li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
+              <li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
+              <li><a className="dribbble" href="#"><i className="fa fa-dribbble"></i></a></li>
+              <li><a className="linkedin" href="#"><i className="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer
