a30536090cb6c33a68785ca44b280fc892de742d
Ajal Jose <ajaljosem@gmail.com>
2023-07-25 20:42:06 +0530

    about page design added

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-
+import './About.css';
 function About() {
   return (
-    <div>
-        <h1>About Porsche</h1>
+    <div className='about'>
+        <h1 >About</h1>
         <p>The Porsche Taycan Turbo S is an all-electric sports car produced by the German automaker Porsche. It features a sleek and aerodynamic design, with a low stance and distinctive Porsche styling cues. The Turbo S model is the top-of-the-line variant in the Taycan lineup and boasts impressive performance capabilities.
 
 Under the hood, the Taycan Turbo S is powered by a pair of electric motors that deliver a combined output of 750 horsepower and 774 lb-ft of torque. This allows the car to accelerate from 0 to 60 mph in just 2.6 seconds and reach a top speed of 162 mph.</p>
-    <label htmlFor="#feedback">Feedback</label> <textarea name="" id="feedback" cols="30" rows="10"></textarea>
+    <h2 >Feedback</h2>
+    <div className="input-container">
+      <input type="text" id="input" required />
+      <label htmlFor="input" className="label">
+        Name
+      </label>
+      <div className="underline"></div>
+    </div>
+    <div className="input-container">
+      <input type="text" id="input2" required />
+      <label htmlFor="input2" className="label">
+        email@here.com
+      </label>
+      <div className="underline"></div>
+    </div>
+    <div className="input-container">
+    <textarea name="" id="feedback" rows="10" cols="50" placeholder='Message...'></textarea>
+
+    </div>
+    <div className="input-container">
+    <button>Submit</button>
+
+    </div>
     </div>
   )
 }
