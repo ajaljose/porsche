b9a89f5c7ebf43851b36e5b21a2b1d3867ceb792
Ajal Jose <ajaljosem@gmail.com>
2024-04-17 20:56:03 +0530

    porsche navbar

--- src/home/navbar.js
+++ src/home/navbar.js
@@ -8,13 +8,20 @@ const Navbar = () => {
     <div className="nav foreground">
       <img src={logo} alt="porsche" width="50" height="50" title="PORSCHE" />
       <h3 className="prcFont">PORSCHE</h3>
-      <h4 className="navMenu prcFont">Menu <img className="settings" src={hamlogo} onClick={() => {
-  const rightNavbar = document.querySelector('.right-navbar');
-  rightNavbar.style.right = '0';
-  rightNavbar.style.transition = 'right 0.6s ease-in-out'; // adjust the transition duration and timing function to your preference
-}}
-
-/></h4>
+      <div className="sidemenu">
+        <h4>Models</h4>
+        <h4 className="navMenu prcFont">
+          <img
+            className="settings"
+            src={hamlogo}
+            onClick={() => {
+              const rightNavbar = document.querySelector(".right-navbar");
+              rightNavbar.style.right = "0";
+              rightNavbar.style.transition = "right 0.6s ease-in-out"; // adjust the transition duration and timing function to your preference
+            }}
+          />
+        </h4>
+      </div>
     </div>
   );
 };
