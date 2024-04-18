d99e8342eecd8775c8aec551408b4ed72b69224a
Ajal Jose <ajaljosem@gmail.com>
2024-04-18 11:38:30 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Navbar from './home/navbar';
-import Home from './home/home.js';
-import CenterBgLogo from './home/centerBgLogo.jsx';
-import Details from './home/details';
-import ImageSlider from './home/imageSlider';
-import RightNavbar from './home/rightNavbar';
-import Footer from './home/footer';
-import { useState } from 'react';
-import About from './about/About';
-import Gallery from './gallery/Gallery';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Navbar from "./home/navbar";
+import Home from "./home/home.js";
+import CenterBgLogo from "./home/centerBgLogo.jsx";
+import Details from "./home/details";
+import ImageSlider from "./home/imageSlider";
+import RightNavbar from "./home/rightNavbar";
+import Footer from "./home/footer";
+import { useState } from "react";
+import About from "./about/About";
+import Gallery from "./gallery/Gallery";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const images = [
-  'https://files.porsche.com/filestore/image/middle-east/none/homepage-banner-india/normal/c2e54b1f-9285-11e9-80c4-005056bbdc38/porsche-normal.jpg',
-  'https://imgd-ct.aeplcdn.com/664x415/n/cw/ec/24421/718-exterior-left-front-three-quarter-78299.jpeg?q=75',
-  'https://files.porsche.com/filestore/image/multimedia/none/ww-rangeshotbanner-front/normal/5fda392c-d2e3-11e9-80c5-005056bbdc38/porsche-normal.jpg',
+  "https://files.porsche.com/filestore/image/middle-east/none/homepage-banner-india/normal/c2e54b1f-9285-11e9-80c4-005056bbdc38/porsche-normal.jpg",
+  "https://imgd-ct.aeplcdn.com/664x415/n/cw/ec/24421/718-exterior-left-front-three-quarter-78299.jpeg?q=75",
+  "https://files.porsche.com/filestore/image/multimedia/none/ww-rangeshotbanner-front/normal/5fda392c-d2e3-11e9-80c5-005056bbdc38/porsche-normal.jpg",
 ];
 function App() {
-    const [page,setPage]=useState();
-    let component;
-    if(page=='about'){
-        component=<><Navbar />
-        <About/>
-        <CenterBgLogo />
-        <RightNavbar page={page} setPage={setPage} />
-        <Footer />
-        </>;
-    }else if(page=='gallery'){
-      component=<><Navbar />
-      <Gallery/>
+  const [page, setPage] = useState("home");
+  let component;
+  if (page == "about") {
+    component = (
+      <>
+        <About />
+      </>
+    );
+  } else if (page == "gallery") {
+    component = (
+      <>
+        <Gallery />
+      </>
+    );
+  } else if (page == "home"){
+    component = (
+      <>
+        <Home />
+        <Details />
+        <ImageSlider images={images} />
+      </>
+    );
+  }
+  return (
+    <div>
+      <Navbar setPage={setPage} />
+      {component}
       <CenterBgLogo />
       <RightNavbar page={page} setPage={setPage} />
       <Footer />
-      </>;
-    }else{
-        component = (
-            <>
-              <Navbar />
-              <Home />
-              <CenterBgLogo />
-              <Details />
-              <RightNavbar page={page} setPage={setPage} />
-              <ImageSlider images={images} />
-              <Footer />
-            </>
-          );
-    }
-  return (
-    <div>
-    {component}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/home/navbar.js
+++ src/home/navbar.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import "./css/navbar.css";
 import logo from "../images/porschelogo.png";
 import hamlogo from "../images/hamburger.png";
 // import font from url('https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Open+Sans&family=Raleway:wght@100&display=swap');
 
-const Navbar = () => {
+const Navbar = ({setPage }) => {
+  const setNavPage=(item)=>{
+    setPage(item);
+  }
   return (
     <div className="nav foreground">
       <img src={logo} alt="porsche" width="50" height="50" title="PORSCHE" />
       <h3 className="prcFont">PORSCHE</h3>
       <div className="sidemenu">
-        <h4>Models</h4>
+      <h4 onClick={()=>setNavPage('home')}>Home</h4>
+        <h4 onClick={()=>setNavPage('model')}>Models</h4>
         <h4 className="navMenu prcFont">
           <img
             className="settings"
